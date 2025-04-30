07ee16b31b2f14677e6b9c36cd1492f587acef82
mgering <mgering@mail.de>
2025-04-30 13:02:12 +0200

    jsut to safe

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,11 @@
+#include "ScalarConverter.hpp"
+
+//stoi() convert string to int (used since c++11), atoi() is (C-Style)
+//stof() or atof() convert string to float
+//stod() convert string to double
+
+
+void ScalarConverter::convert(const std::string& literal)
+{
+	
+}
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class ScalarConverter {
+private:
+	ScalarConverter() = delete;
+	~ScalarConverter() = delete;
+	ScalarConverter(const ScalarConverter&) = delete;
+	ScalarConverter& operator=(const ScalarConverter&) = delete;
+
+public:
+	static void convert(const std::string& literal);
+};
